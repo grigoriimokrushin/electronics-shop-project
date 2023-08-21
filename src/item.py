ccaca290d1c6a40b005a9c9c016b57f448bffc56
Grigory Mokrushin <wedass@mail.ru>
2023-08-21 21:41:36 +0700

    Homework2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -9,13 +12,40 @@ class Item:
         """
         Создание экземпляра класса item.
 
-        :param name: Название товара.
+        :param name: Название товара (приватный).
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+
+    @property
+    def name(self):
+        """Возвращает название товара, к атрибуту можно обращаться без()."""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Метод срабатывает при операции присваивания,
+        проверяет, что длинна имени не больше 10 знаков.
+        """
+        if len(name) < 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Инициализирует экземпляры класса Item данными из файла src/items.csv"""
+        with open('C:\\Users\\tanya\\PycharmProjects\\electronics-shop-project\\src\\items.csv') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for line in reader:
+                cls.all.append(cls(line['name'], line['price'], line['quantity']))
+
+
 
     def calculate_total_price(self) -> float:
         """
@@ -30,3 +60,8 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @staticmethod
+    def string_to_number(line: str) -> int:
+        """Возвращает число из числа-строки"""
+        return int(float(line))
--- tests/test_item.py
+++ tests/test_item.py
@@ -5,10 +5,12 @@ from src.item import Item
 
 @pytest.fixture
 def item_smartphone():
+    """Зададим фикстуру с экземпляром класса."""
     return Item("Смартфон", 10000, 20)
 
 
 def test_item_init(item_smartphone):
+    """Проверим все атрибуты класса на соотведствие."""
     assert item_smartphone.name == "Смартфон"
     assert item_smartphone.price == 10000
     assert item_smartphone.quantity == 20
@@ -16,11 +18,46 @@ def test_item_init(item_smartphone):
 
 
 def test_calculate_total_price(item_smartphone):
+    """Проверим метод для подсчета общей суммы остатка в магазине."""
     assert item_smartphone.calculate_total_price() == 200000
 
 
 def test_apply_discount():
+    """Проверим метод для реализации скидки."""
     item1 = Item("Смартфон", 10000, 20)
     Item.pay_rate = 0.8
     item1.apply_discount()
     assert item1.price == 8000.0
+
+def test_name(item_smartphone):
+    """
+    Проверяем как записывается name через setter
+    и идет обращение через getter.
+    """
+    item_smartphone.name = '123'
+    assert item_smartphone.name == '123'
+    item_smartphone.name = 'СуперСмартфон'
+    assert item_smartphone.name == 'СуперСмарт'
+
+def test_string_to_number():
+    """
+    Проверяем как работает статический
+    метод для вывода числа из числа-строки.
+    """
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+def test_instantiate_from_csv():
+    """
+    Проверяем как работает метод для
+    инициализации экзепляров из csv-фаил.
+    """
+
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+
